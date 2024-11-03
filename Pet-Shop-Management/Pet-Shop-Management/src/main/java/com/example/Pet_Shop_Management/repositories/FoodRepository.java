49af57ac65528f1bf7b1d068a28dd2b07181e1c4
Nuzhat98 <86733602+Nuzhat98@users.noreply.github.com>
2024-11-03 14:31:38 +0600

    Update FoodRepository.java

--- Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/FoodRepository.java
+++ Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/FoodRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
 public interface FoodRepository extends JpaRepository<FoodEntity, Long> {
-    List<FoodEntity> findByName(String foodName);
+    List<FoodEntity> findByFoodName(String foodName);
 }
