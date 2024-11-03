a025e6d1261bf04bf99b598eab4ee4e267494e76
Nuzhat98 <86733602+Nuzhat98@users.noreply.github.com>
2024-11-03 14:30:14 +0600

    Update CustomerRepository.java

--- Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/CustomerRepository.java
+++ Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/CustomerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-    List<CustomerEntity> findByName(String customerName);
+    List<CustomerEntity> findByCustomerName(String customerName);
 }
