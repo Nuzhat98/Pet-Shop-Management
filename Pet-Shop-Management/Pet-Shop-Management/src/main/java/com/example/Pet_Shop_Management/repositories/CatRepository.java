77fab872bf7d8c5d15ffd72d995bf4bc283b3bed
Nuzhat98 <86733602+Nuzhat98@users.noreply.github.com>
2024-11-03 14:29:40 +0600

    Update CatRepository.java

--- Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/CatRepository.java
+++ Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/CatRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CatRepository extends JpaRepository<CatEntity, Long> {
-    List<CatEntity> findByName(String catName);
+    List<CatEntity> findByCatName(String catName);
 }
