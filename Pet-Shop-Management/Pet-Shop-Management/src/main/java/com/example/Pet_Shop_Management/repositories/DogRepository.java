cf9a97fa909f9866c63a434b173a68a29100901e
Nuzhat98 <86733602+Nuzhat98@users.noreply.github.com>
2024-11-03 14:30:38 +0600

    Update DogRepository.java

--- Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/DogRepository.java
+++ Pet-Shop-Management/Pet-Shop-Management/src/main/java/com/example/Pet_Shop_Management/repositories/DogRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DogRepository extends JpaRepository<DogEntity, Long> {
-    List<DogEntity> findByName(String dogName);
+    List<DogEntity> findByDogName(String dogName);
 }
